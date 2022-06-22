d5801425c9a672a0be2b68081b7d60307fc22407
diegomaldonadodev <md.maldonadodiego@gmail.com>
2022-06-22 17:58:10 -0300

    Desafio 2

--- DesafioBackend.js
+++ DesafioBackend.js
@@ -1,53 +1,118 @@
-class Usuario {
-    // Constructor
-    constructor(nombre, apellido, libros, mascotas) {
-      this.nombre = nombre;
-      this.apellido = apellido;
-      this.libros = [];
-      this.mascotas = [];
-    }
-  
-    // Metodos
-    getFullName() {
-      return `Hola, soy ${this.nombre} ${this.apellido}`;
-    }
-  
-    addMascota(nuevaMascota) {
-      this.mascotas.push(nuevaMascota);
-    }
-  
-    countMascotas() {
-      return this.mascotas.length;
-    }
-  
-    addBook(nombre, autor) {
-      this.libros.push({ nombre: nombre, autor: autor });
-    }
-  
-    getBookNames() {
-      const librosUsuario = [];
-      this.libros.forEach(libro => {
-        librosUsuario.push(libro.nombre);
-      });
-      return librosUsuario;
+const fs = require("fs");
+
+class Contenedor {
+  constructor(nombreDelArchivo) {
+    this.nombreDelArchivo = nombreDelArchivo;
+  }
+
+  async save(guardarObjeto) {
+    try {
+      const resp = await fs.promises.readFile(this.nombreDelArchivo, "utf-8");
+      if (resp == "") {
+        guardarObjeto.id = 1;
+        const arrayObj = [guardarObjeto];
+        await console.log(guardarObjeto.id);
+      } else {
+        const datosObj = JSON.parse(resp);
+        guardarObjeto.id = datosObj.length + 1;
+        datosObj.push(guardarObjeto);
+        await fs.promises.writeFile(
+          this.nombreDelArchivo,
+          JSON.stringify(datosObj)
+        );
+        return console.log(guardarObjeto.id);
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
     }
   }
-  
-  // Creacion de objeto
-  
-  let usuario = new Usuario("Diego", 'Maldonado');
-  
-  // Invocacion de metodos
-  
-  usuario.getFullName();
-  usuario.addMascota('Pochi');
-  usuario.addMascota('Mateo');
-  usuario.addMascota('Dexter');
-  usuario.countMascotas();
-  usuario.addBook("El tao del jet kune do", "Bruce Lee");
-  usuario.addBook("El arte de la guerra", "Sun Tzu");
-  usuario.getBookNames();
-  
-  // Utilizados en un console.log();
-  
-  console.log(usuario.getFullName() + ' y tengo ' + usuario.countMascotas() + ' mascotas. Sus nombres son: ' + usuario.mascotas[0] + ', ' + usuario.mascotas[1] + ' y ' + usuario.mascotas[2]  + '. Me gusta leer y los ultimos dos libros que lei fueron ' + usuario.getBookNames());
+
+  async getById(id) {
+    try {
+      const resp = await fs.promises.readFile(this.nombreDelArchivo, "utf-8");
+      const data = JSON.parse(resp);
+      const resultado = data.findIndex((elemento) => elemento.id === id);
+      if (resultado >= 0) {
+        return console.log(data[resultado]);
+      } else {
+        return console.log(null);
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  }
+
+  async getAll() {
+    try {
+      const resp = await fs.promises.readFile(this.nombreDelArchivo, "utf-8");
+      const data = await JSON.parse(resp);
+      return console.log(data);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      const resp = await fs.promises.readFile(this.nombreDelArchivo, "utf-8");
+      const data = await JSON.parse(resp);
+      const objEliminado = data.splice(id - 1, 1);
+      if (objEliminado.length > 0) {
+        await fs.promises.writeFile(
+          this.nombreDelArchivo,
+          JSON.stringify(JSON.stringify(datos))
+        );
+        return console.log("Objeto eliminado:\n", objEliminado);
+      } else {
+        console.log("El objeto no existe");
+      }
+    } catch (error) {
+      console.log(`Error:${error}`);
+    }
+  }
+
+  async deleteAll() {
+    try {
+      await fs.promises.writeFile(this.nombreDelArchivo, "");
+      return console.log(`Se elimino todo de: ${this.nombreDelArchivo}`);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  }
+}
+
+const contenedor1 = new Contenedor("productos.txt");
+
+const producto1 = {
+  nombre: "PROCESADOR INTEL CORE I7 10700F 4.8 GHZ COMET LAKE 1200 SIN GPU",
+  precio: 49000,
+  imagen: "https://mexx-img-2019.s3.amazonaws.com/39187_1.jpeg",
+};
+
+const producto2 = {
+  nombre: "NOTEBOOK GAMER GIGABYTE AORUS I7 32GB SSD 512GB RTX 3080Q 8GB WIN10",
+  precio: 505700,
+  imagen: "https://mexx-img-2019.s3.amazonaws.com/notebook-gamer_42169_1.jpeg",
+};
+
+const producto3 = {
+  nombre: "PLACA DE VIDEO GEFORCE RTX 3090 24GB ASUS ROG STRIX GAMING OC",
+  precio: 460000,
+  imagen:
+    "https://mexx-img-2019.s3.amazonaws.com/Placa-de-video-rtx-3090-asus_39206_1.jpeg",
+};
+
+setTimeout(() => {
+  contenedor1.save(producto1);
+}, 500);
+setTimeout(() => {
+  contenedor1.save(producto2);
+}, 1000);
+
+setTimeout(() => {
+  contenedor1.save(producto3);
+}, 1500);
+
+setTimeout(() => {
+  contenedor1.getById(3);
+}, 2000);
---
+++ Desafios/DesafioBackend1.js
@@ -0,0 +1,53 @@
+class Usuario {
+    // Constructor
+    constructor(nombre, apellido, libros, mascotas) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.libros = [];
+      this.mascotas = [];
+    }
+  
+    // Metodos
+    getFullName() {
+      return `Hola, soy ${this.nombre} ${this.apellido}`;
+    }
+  
+    addMascota(nuevaMascota) {
+      this.mascotas.push(nuevaMascota);
+    }
+  
+    countMascotas() {
+      return this.mascotas.length;
+    }
+  
+    addBook(nombre, autor) {
+      this.libros.push({ nombre: nombre, autor: autor });
+    }
+  
+    getBookNames() {
+      const librosUsuario = [];
+      this.libros.forEach(libro => {
+        librosUsuario.push(libro.nombre);
+      });
+      return librosUsuario;
+    }
+  }
+  
+  // Creacion de objeto
+  
+  let usuario = new Usuario("Diego", 'Maldonado');
+  
+  // Invocacion de metodos
+  
+  usuario.getFullName();
+  usuario.addMascota('Pochi');
+  usuario.addMascota('Mateo');
+  usuario.addMascota('Dexter');
+  usuario.countMascotas();
+  usuario.addBook("El tao del jet kune do", "Bruce Lee");
+  usuario.addBook("El arte de la guerra", "Sun Tzu");
+  usuario.getBookNames();
+  
+  // Utilizados en un console.log();
+  
+  console.log(usuario.getFullName() + ' y tengo ' + usuario.countMascotas() + ' mascotas. Sus nombres son: ' + usuario.mascotas[0] + ', ' + usuario.mascotas[1] + ' y ' + usuario.mascotas[2]  + '. Me gusta leer y los ultimos dos libros que lei fueron ' + usuario.getBookNames());
